653b67cb73d1edf932e019add3926a49c6bdae74
Mahesh-Abeykoon <mahesh.a@affiniti.biz>
2023-08-30 14:54:22 +0530

    Updates

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const https = require("https");
-
 const bodyParser = require("body-parser");
+const path = require("path");
 
 const app = express();
 var port = 5002;
@@ -14,29 +14,42 @@ app.get("/",function(req, res){
     
 })
 
-app.post("/", function(req, res){
-    
+app.post("/", function (req, res) {
     const city = req.body.cityName;
     const apiKey = "104bd1d6662e615d5091b5f85fe6e4f8";
-    const url = "https://api.openweathermap.org/data/2.5/weather?q="+city+"&units=metric&appid="+apiKey;
+    const url = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&appid=" + apiKey;
 
-    https.get(url, function(resp){
-        console.log(resp); 
+    https.get(url, function (resp) {
+        let responseData = "";
 
-        resp.on("data", function(data){
-            const weatherData = JSON.parse(data);
-            const temp = weatherData.main.temp
-            const weatherDescription = weatherData.weather[0].description
-            const icon = weatherData.weather[0].icon
-            const imageURL = "https://openweathermap.org/img/wn/" + icon + "@2x.png"
-    
-            res.write("Weather is currently "+ weatherDescription)
-            res.write(". The temparture in " +city+ "is "+temp);
-            res.write("<img src=" + imageURL + ">");
-            res.send();
-        })
-    })
-})
+        resp.on("data", function (data) {
+            responseData += data;
+        });
+
+        resp.on("end", function () {
+            const weatherData = JSON.parse(responseData);
+
+            if (weatherData.cod === 200) {
+                const temp = weatherData.main.temp;
+                const weatherDescription = weatherData.weather[0].description;
+                const icon = weatherData.weather[0].icon;
+                const imageURL = "https://openweathermap.org/img/wn/" + icon + "@2x.png";
+
+                // Replace placeholders in the HTML content with actual data
+                const weatherHTML = `
+                    ${require('fs').readFileSync(path.join(__dirname, "public", "weather.html"), "utf8")}
+                `.replace("{CITY}", city)
+                .replace("{DESCRIPTION}", weatherDescription)
+                .replace("{TEMPERATURE}", temp)
+                .replace("{IMAGE_URL}", imageURL);
+
+                res.send(weatherHTML);
+            } else {
+                res.sendFile(path.join(__dirname, "public", "error.html"));
+            }
+        });
+    });
+});
 
 
 app.listen(port, function(){
